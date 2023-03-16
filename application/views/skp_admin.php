0b19c13a1d4b91ee21e89c8fc32fe62fcd32abb1
Rizal Purnomo <rizalpurnomo@gmail.com>
2023-03-16 08:52:29 +0700

    export excel

--- application/views/skp_admin.php
+++ application/views/skp_admin.php
@@ -11,6 +11,10 @@
 		href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback" />
 	<!-- Font Awesome -->
 	<link rel="stylesheet" href="<?php echo base_url() ?>assets/plugins/fontawesome-free/css/all.min.css" />
+	<!-- DataTables -->
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
 	<!-- daterange picker -->
 	<link rel="stylesheet" href="<?php echo base_url() ?>assets/plugins/daterangepicker/daterangepicker.css" />
 	<!-- iCheck for checkboxes and radio inputs -->
@@ -316,7 +320,7 @@
 										<div class="card-header">
 											<div class="row">
 												<div class="col-md-12">
-													<?php echo form_open('skp/adminSkp'); ?>
+													<?php echo form_open('admin/Skp'); ?>
 													<div class="form-group">
 														<label>Tanggal</label>
 														<div class="input-group date" id="reservationdate"
@@ -344,7 +348,7 @@
 															<?php } ?>
 														</select>
 													</div>
-													<button type="submit" class="btn btn-primary">Submit</button>
+													<button type="submit" name="submit" class="btn btn-primary">Submit</button>
 													<?php form_close(); ?>
 												</div>
 											</div>
@@ -449,6 +453,19 @@
 	<script src="<?php echo base_url() ?>assets/plugins/jquery/jquery.min.js"></script>
 	<!-- Bootstrap 4 -->
 	<script src="<?php echo base_url() ?>assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<!-- DataTables  & Plugins -->
+	<script src="<?php echo base_url() ?>assets/plugins/datatables/jquery.dataTables.min.js"></script>
+	<script src="<?php echo base_url() ?>assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+	<script src="<?php echo base_url() ?>assets/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+	<script src="<?php echo base_url() ?>assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+	<script src="<?php echo base_url() ?>assets/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+	<script src="<?php echo base_url() ?>assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+	<script src="<?php echo base_url() ?>assets/plugins/jszip/jszip.min.js"></script>
+	<script src="<?php echo base_url() ?>assets/plugins/pdfmake/pdfmake.min.js"></script>
+	<script src="<?php echo base_url() ?>assets/plugins/pdfmake/vfs_fonts.js"></script>
+	<script src="<?php echo base_url() ?>assets/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+	<script src="<?php echo base_url() ?>assets/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+	<script src="<?php echo base_url() ?>assets/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>	
 	<!-- Select2 -->
 	<script src="<?php echo base_url() ?>assets/plugins/select2/js/select2.full.min.js"></script>
 	<!-- Bootstrap4 Duallistbox -->
@@ -476,6 +493,21 @@
 	<!-- <script src="<?php echo base_url() ?>assets/dist/js/demo.js"></script> -->
 	<!-- Page specific script -->
 	<script>
+		  	$(function () {
+				$("#example1").DataTable({
+					"responsive": true, "lengthChange": false, "autoWidth": false,
+					"buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+				}).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+				$('#example2').DataTable({
+					"paging": true,
+					"lengthChange": false,
+					"searching": false,
+					"ordering": true,
+					"info": true,
+					"autoWidth": false,
+					"responsive": true,
+				});
+			});
 		$(function () {
 			//Initialize Select2 Elements
 			$(".select2").select2();
