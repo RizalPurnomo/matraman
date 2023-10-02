cfdebfb3244177d546c7b8e9b3c727ecb57b06b4
Rizal Purnomo <rizalpurnomo@gmail.com>
2023-10-02 11:57:26 +0700

    KIA jadi antrian prioritas

--- application/modules/skp/controllers/Skp.php
+++ application/modules/skp/controllers/Skp.php
@@ -45,8 +45,8 @@ class Skp extends MY_Controller
     {
         $is_umum = 0;
         $is_lansia = 0;
-        $umum = array(1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24);
-        $lansia = array(8, 5, 15, 16, 2, 20, 18, 7, 14);
+        $umum = array(1, 2, 3, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24);
+        $lansia = array(4, 8, 5, 15, 16, 2, 20, 18, 7, 14);
 
         if (in_array($id, $umum)) {
             $is_umum = 1;
