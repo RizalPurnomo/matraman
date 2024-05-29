37fd08e42910e254af5e59e71fe5d0b7eda16e70
Rizal Purnomo <rizalpurnomo@gmail.com>
2024-05-29 14:27:38 +0700

    tambah pengumuman ilp dan up24

--- application/modules/speaker/views/speaker.php
+++ application/modules/speaker/views/speaker.php
@@ -86,6 +86,32 @@
                         Your browser does not support the audio element.
                     </audio>`;
                 lbl.innerHTML = "HIMBAUAN PEMAKAIAN MASKER";
+
+
+                //ILP
+            } else if (e.innerHTML == "Senin\n07:00:00" || e.innerHTML == "Selasa\n07:00:00" || e.innerHTML == "Rabu\n07:00:00" || e.innerHTML == "Kamis\n07:00:00" || e.innerHTML == "Jumat\n07:00:00" || e.innerHTML == "Sabtu\n07:00:00" || e.innerHTML == "Minggu\n07:00:00" ||
+                e.innerHTML == "Senin\n09:00:00" || e.innerHTML == "Selasa\n09:00:00" || e.innerHTML == "Rabu\n09:00:00" || e.innerHTML == "Kamis\n09:00:00" || e.innerHTML == "Jumat\n09:00:00" || e.innerHTML == "Sabtu\n09:00:00" || e.innerHTML == "Minggu\n09:00:00" ||
+                e.innerHTML == "Senin\n11:00:00" || e.innerHTML == "Selasa\n11:00:00" || e.innerHTML == "Rabu\n11:00:00" || e.innerHTML == "Kamis\n11:00:00" || e.innerHTML == "Jumat\n11:00:00" || e.innerHTML == "Sabtu\n11:00:00" || e.innerHTML == "Minggu\n11:00:00" ||
+                e.innerHTML == "Senin\n13:00:00" || e.innerHTML == "Selasa\n13:00:00" || e.innerHTML == "Rabu\n13:00:00" || e.innerHTML == "Kamis\n13:00:00" || e.innerHTML == "Jumat\n13:00:00" || e.innerHTML == "Sabtu\n13:00:00" || e.innerHTML == "Minggu\n13:00:00") {
+                aud.innerHTML = `<audio id="myAudio" controls autoplay>
+                        <source src="<?php echo base_url(); ?>assets/upload/speaker/pengumuman-ilp.mp3" type="audio/mpeg">
+                        Your browser does not support the audio element.
+                    </audio>`;
+                lbl.innerHTML = "PENGUMUMAN ILP";
+
+
+                //UP24
+            } else if (e.innerHTML == "Senin\n14:30:00" || e.innerHTML == "Selasa\n14:30:00" || e.innerHTML == "Rabu\n14:30:00" || e.innerHTML == "Kamis\n14:30:00" || e.innerHTML == "Jumat\n14:30:00" || e.innerHTML == "Sabtu\n14:30:00" || e.innerHTML == "Minggu\n14:30:00" ||
+                e.innerHTML == "Senin\n16:30:00" || e.innerHTML == "Selasa\n16:30:00" || e.innerHTML == "Rabu\n16:30:00" || e.innerHTML == "Kamis\n16:30:00" || e.innerHTML == "Jumat\n16:30:00" || e.innerHTML == "Sabtu\n16:30:00" || e.innerHTML == "Minggu\n16:30:00" ||
+                e.innerHTML == "Senin\n18:30:00" || e.innerHTML == "Selasa\n18:30:00" || e.innerHTML == "Rabu\n18:30:00" || e.innerHTML == "Kamis\n18:30:00" || e.innerHTML == "Jumat\n18:30:00" || e.innerHTML == "Sabtu\n18:30:00" || e.innerHTML == "Minggu\n18:30:00" ||
+                e.innerHTML == "Senin\n20:30:00" || e.innerHTML == "Selasa\n20:30:00" || e.innerHTML == "Rabu\n20:30:00" || e.innerHTML == "Kamis\n20:30:00" || e.innerHTML == "Jumat\n20:30:00" || e.innerHTML == "Sabtu\n20:30:00" || e.innerHTML == "Minggu\n20:30:00" ||
+                e.innerHTML == "Senin\n22:30:00" || e.innerHTML == "Selasa\n22:30:00" || e.innerHTML == "Rabu\n22:30:00" || e.innerHTML == "Kamis\n22:30:00" || e.innerHTML == "Jumat\n22:30:00" || e.innerHTML == "Sabtu\n22:30:00" || e.innerHTML == "Minggu\n22:30:00") {
+                aud.innerHTML = `<audio id="myAudio" controls autoplay>
+                        <source src="<?php echo base_url(); ?>assets/upload/speaker/pengumuman-up24.mp3" type="audio/mpeg">
+                        Your browser does not support the audio element.
+                    </audio>`;
+                lbl.innerHTML = "PENGUMUMAN UP24";
+                //test
             }
 
             setTimeout('jam()', 1000);
